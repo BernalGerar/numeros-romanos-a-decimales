67ab1d6a99593c4f3da6cf4917401b82ac245a73
David Bernal <david.ber.1530@gmail.com>
2022-01-09 13:49:03 -0300

    first commit

---
+++ romanos.js
@@ -0,0 +1,13 @@
+
+const Number_r = (function(menssage){
+    const methods = {
+      public: {},
+      private: {}
+    };
+
+    public.test = function(number) {
+      
+    };
+
+    return methods.public;
+})(console.log);
