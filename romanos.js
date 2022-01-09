f18f9ea7a21246308212209f3f3fbc45ea7db78e
David Bernal <david.ber.1530@gmail.com>
2022-01-09 15:16:58 -0300

    solo se puede contar en romano hasta el 3999

--- romanos.js
+++ romanos.js
@@ -5,9 +5,42 @@ const Number_r = (function(menssage){
       private: {}
     };
 
-    public.test = function(number) {
+    const public = methods.public;
+    const private = methods.private;
+
+    private.values = {
+      "I": 1,
+      "V": 5,
+      "X": 10,
+      "L": 50,
+      "C": 100,
+      "D": 500,
+      "M": 1000
+    };
+
+    private.number = /([IVXLCDM]+)/g;
+    
+    private.test = function(number) {
       
     };
 
-    return methods.public;
+    public.decimal = function(number) {
+      const val = number.split("");
+      var decimal = 0;
+      
+      for(let i = 0; i < val.length; i++) {
+        if( private.values[ val[i] ] < private.values[ val[i + 1] ] ) {
+          const num1 = val.splice(i, 1)[0];
+          const num2 = val.splice(i, 1)[0];
+          decimal += private.values[ num2 ] - private.values[ num1 ];
+          i--;
+        }else decimal += private.values[ val[i] ];
+      };
+
+      return decimal;
+    };
+
+    return public;
 })(console.log);
+
+//console.log(Number_r.decimal("XXX"))
